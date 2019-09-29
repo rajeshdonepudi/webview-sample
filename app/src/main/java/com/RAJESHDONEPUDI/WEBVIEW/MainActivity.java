08fdd24efe7a4913ee6837d7b73f0eb652394860
rajesh <rajeshdonepudi1@gmail.com>
2019-09-29 10:13:58 +0530

    Initial commit

---
+++ app/src/main/java/com/RAJESHDONEPUDI/WEBVIEW/MainActivity.java
@@ -0,0 +1,27 @@
+package com.RAJESHDONEPUDI.WEBVIEW;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = (WebView) findViewById(R.id.webView);
+        view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl("http://google.com");
+    }
+}
